2cb6ec7de4a29c77974922d9779a19e9e7a7fde3
Kanishkar Bubeshkumar <kanishkarbubesh@gmail.com>
2020-05-24 15:51:50 +0530

    Version 1.0

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ ReactDOM.render(
         <br />
         <br />
         <br />
-        <br />
-        <Jumbo></Jumbo>
         <App />
         <Footerpart></Footerpart>
     </React.StrictMode>,
